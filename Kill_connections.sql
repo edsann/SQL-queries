ce5cc49f7a865b1eac72827e999ab7b4ecfde2db
Edoardo Sanna <Edoardo.sanna@gmx.com>
2019-08-06 13:52:29 +0200

    Create Kill_connections.sql

---
+++ Kill_connections.sql
@@ -0,0 +1,19 @@
+		/* Kill connections and set to single user mode */
+    
+		USE Database_name
+		GO
+		ALTER DATABASE Database_name
+		SET SINGLE_USER
+		WITH ROLLBACK IMMEDIATE
+		GO
+ 
+		/* 
+		Do your operations, e.g. restore files or filegroups 
+		*/
+ 
+		/* Set the database back in to multiple user mode */
+		USE Database_name
+		GO
+		ALTER DATABASE Database_name 
+		SET MULTI_USER
+		GO
