6d1bf03a02f2f136825c9d354866041aaaef7063
Edoardo Sanna <Edoardo.sanna@gmx.com>
2019-08-06 12:28:37 +0200

    Update Table_name.sql

--- Table_name.sql
+++ Table_name.sql
@@ -1,5 +1,5 @@
 /* Cercare una tabella nel database filtrando per descrizione */ 
 
-USE Mrt						/* Database */
+USE <Database_name>						/* Database */
 SELECT * FROM information_schema.tables		/* Tabelle presenti nello schema di database */
 WHERE Table_name LIKE '%%'			/* Filtro sul nome tabella */
