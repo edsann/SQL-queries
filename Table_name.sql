ae78f7fc7fa6c8a12e43a9884c4b81801eca02a5
Edoardo Sanna <Edoardo.sanna@gmx.com>
2019-08-22 08:43:28 +0200

    Update Table_name.sql

--- Table_name.sql
+++ Table_name.sql
@@ -1,5 +1,5 @@
 /* Search a table in the database by name */ 
 
-USE <Database_name>						/* Database */
-SELECT * FROM information_schema.tables		/* Tables in the database schema */
-WHERE Table_name LIKE '%%'			/* Filter on the table name */
+USE DATABASE -- Insert database name
+SELECT * FROM information_schema.tables -- Tables in the database schema
+WHERE Table_name LIKE '%%' -- Filter on the table name			
