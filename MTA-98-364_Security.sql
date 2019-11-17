d48af5ad1d2686ee100008dbee0312ab56813ef1
Edoardo Sanna <Edoardo.sanna@gmx.com>
2019-11-17 17:48:12 +0100

    Create MTA-98-364_Security.sql

---
+++ MTA-98-364_Security.sql
@@ -0,0 +1,10 @@
+/* Notes
+
+SECURABLES - Objects (databases, tables, softwares, etc.) to be secured
+PRINCIPAL - Users, groups and other individuals having access to Securables
+PERMISSIONS - Authorizations, to be granted or revoked, of Principals to access Securables
+	Permissions levels:
+		Server Security (Windows Auth. - SQL Auth.)
+		Access databases
+		Access objects
+
