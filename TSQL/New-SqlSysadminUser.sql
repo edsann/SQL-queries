bf38d4cb21261335edeb361ef2d61b9c62626bc5
Edoardo Sanna <edoardo.sanna.es@gmail.com>
2020-11-23 14:31:10 +0100

    Create New-SqlSysadminUser.sql

---
+++ TSQL/New-SqlSysadminUser.sql
@@ -0,0 +1,11 @@
+/* New-SqlSysadminUser.sql
+/*
+/* It creates a new sysadmin login with the specified password */
+
+USE YourDatabaseName
+-- Creates new login
+CREATE LOGIN YourUsername WITH PASSWORD = 'YourPassword';
+GO
+-- Assigns the sysadmin server role
+EXEC master..sp_addsrvrolemember @loginame = N'YourUsername', @rolename = N'sysadmin'
+GO
