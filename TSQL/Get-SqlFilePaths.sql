22d3eb941781ae716b677a5145e2effe98a25c8e
Edoardo Sanna <edoardo.sanna.es@gmail.com>
2020-11-23 14:21:21 +0100

    Update and rename Get-File-list.sql to Get-SqlFilePaths.sql

--- TSQL/Get-SqlFilePaths.sql
+++ TSQL/Get-SqlFilePaths.sql
@@ -1,5 +1,7 @@
+/* Get-SqlFilePaths.sql
+/*
 /* Returns a list of all the logical and physical names for the files for every single database on the SQL Server Instance */
-/*https://blog.sqlauthority.com/2018/11/19/sql-server-get-list-of-the-logical-and-physical-name-of-the-files-in-the-entire-database/ */
+/* Source : https://blog.sqlauthority.com/2018/11/19/sql-server-get-list-of-the-logical-and-physical-name-of-the-files-in-the-entire-database/ */
 
 SELECT 
   d.name DatabaseName, 
