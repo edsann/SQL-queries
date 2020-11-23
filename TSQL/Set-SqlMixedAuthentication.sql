50d768d3bf0166560ac7046297dc0f447beb2e30
Edoardo Sanna <edoardo.sanna.es@gmail.com>
2020-11-23 14:19:05 +0100

    Create Set-SqlMixedAuthentication.sql

---
+++ TSQL/Set-SqlMixedAuthentication.sql
@@ -0,0 +1,15 @@
+/* Set-SqlMixedAuthentication.sql
+/*
+/* The script activates the Mixed Mode Authentication in the current SQL Server instance */
+
+USE [master]
+GO
+/* [Note: 2 indicates mixed mode authentication. 1 is for windows only authentication] */
+EXEC xp_instance_regwrite N'HKEY_LOCAL_MACHINE', 
+     N'Software\Microsoft\MSSQLServer\MSSQLServer',
+     N'LoginMode', REG_DWORD, 2
+GO
+
+/* Restart SQL Server engine */
+
+
