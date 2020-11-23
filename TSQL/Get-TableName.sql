55b518a42f5f5b24a2cb4637f4498ab34a000083
Edoardo Sanna <edoardo.sanna.es@gmail.com>
2020-11-23 14:27:25 +0100

    Update and rename Table_name.sql to Get-TableName.sql

--- TSQL/Get-TableName.sql
+++ TSQL/Get-TableName.sql
@@ -1,5 +1,7 @@
-/* Search a table in the database by name */ 
+/* Get-TableName.sql
+/*
+/* It Searches a table in the database by table name */ 
 
-USE DATABASE -- Insert database name
+USE DatabaseName -- Insert database name
 SELECT * FROM information_schema.tables -- Tables in the database schema
 WHERE Table_name LIKE '%%' -- Filter on the table name			
