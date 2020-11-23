418b354bb9b5d4d0ee18e590aaef1978138e46fa
Edoardo Sanna <edoardo.sanna.es@gmail.com>
2020-11-23 15:04:24 +0100

    Update and rename Different_collations.sql to Join-DifferentCollations.sql

--- Join-DifferentCollations.sql
+++ Join-DifferentCollations.sql
@@ -1,23 +1,16 @@
+/* Join-DifferentCollations.sql
+/*
 /* Joinbetween two DBs with different collations */
 /* No need to specify USE DATABASE_NAME at the beginning because the DBs are specified within the query */
 /* Replace DATABASE*, TABLE*, COLLATE*, FIELD* with your values */
 
-SELECT 
-	* 
-FROM
-	/* Table1 from Database1 */
-	[Database1].[dbo].[Table1] AS T1
-JOIN 
-	/* Table2 from Database2 */
-	[Database2].[dbo].[Table2] AS T2
-ON 
-	/* Keys to use, each one with its own collation */
-		/* E.g. Collate1 = Latin1_General_CI_AS */
-		/* E.g. Collate2 = SQL_Latin1_General_CP850_CI_AI */
-	T1.Field1 COLLATE Collate1 = T2.Field2 COLLATE Collate2
-WHERE 
-	/* Condition */
-	Whatever
+SELECT * 
+FROM [Database1].[dbo].[Table1] AS T1 -- Table1 from Database1 
+JOIN [Database2].[dbo].[Table2] AS T2 -- Table2 from Database2
+ON T1.Field1 COLLATE Collate1 = T2.Field2 COLLATE Collate2 -- Keys to use, each one with its own collation
+	/* E.g. Collate1 = Latin1_General_CI_AS */
+	/* E.g. Collate2 = SQL_Latin1_General_CP850_CI_AI */
+WHERE [CONDITION] -- Insert whatever condition
 
 /* An alternative below: */
 
