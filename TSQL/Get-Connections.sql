c7b87c94dfaced7d5c112dbb7005ffb83b66f703
Edoardo Sanna <edoardo.sanna.es@gmail.com>
2021-01-27 16:44:33 +0100

    Create Get-Connections.sql

---
+++ TSQL/Get-Connections.sql
@@ -0,0 +1,7 @@
+-- Quantità di processi connessi all'istanza
+SELECT hostname, COUNT(hostname) AS Processes
+FROM sys.sysprocesses AS P
+JOIN sys.sysdatabases AS D ON (D.dbid = P.dbid)
+JOIN sys.sysusers AS U ON (P.uid = U.uid)
+GROUP BY hostname
+ORDER BY COUNT(hostname) DESC
