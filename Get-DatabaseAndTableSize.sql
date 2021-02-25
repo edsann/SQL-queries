aeaa24a0bd3ce81744af34f5800c6ca47843e61c
Edoardo Sanna <edoardo.sanna.es@gmail.com>
2021-02-25 09:44:46 +0100

    Update and rename Get-DatabaseSize.sql to Get-DatabaseAndTableSize.sql

---
+++ Get-DatabaseAndTableSize.sql
@@ -0,0 +1,15 @@
+/* MySql */
+
+-- Returns the database sizes in MB
+SELECT 
+  table_schema AS "Database", 
+  ROUND(SUM(data_length + index_length) / 1024 / 1024, 2) AS "Size (MB)" 
+FROM information_schema.TABLES 
+GROUP BY table_schema;
+
+-- Returns the table of a specific DATABASE_NAME
+SELECT table_name AS "Table",
+ROUND(((data_length + index_length) / 1024 / 1024), 2) AS "Size (MB)"
+FROM information_schema.TABLES
+WHERE table_schema = "database_name"
+ORDER BY (data_length + index_length) DESC;
