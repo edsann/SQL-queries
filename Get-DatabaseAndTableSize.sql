53e4c1fa179eaa4547cc9a37c806aa195e79d51d
Edoardo Sanna <edoardo.sanna.es@gmail.com>
2021-03-17 17:20:31 +0100

    Update Get-DatabaseAndTableSize.sql

--- Get-DatabaseAndTableSize.sql
+++ Get-DatabaseAndTableSize.sql
@@ -1,3 +1,17 @@
+/* SQL Server */
+
+-- Returns database Name, Log Size, Row Size, Total Size for current db
+SELECT 
+      [Database Name] = DB_NAME(database_id)
+    , [Log Size (MB)] = CAST(SUM(CASE WHEN type_desc = 'LOG' THEN size END) * 8. / 1024 AS DECIMAL(8,2))
+    , [Row Size (MB)] = CAST(SUM(CASE WHEN type_desc = 'ROWS' THEN size END) * 8. / 1024 AS DECIMAL(8,2))
+    , [Total Size (MB)] = CAST(SUM(size) * 8. / 1024 AS DECIMAL(8,2))
+FROM sys.master_files WITH(NOWAIT)
+WHERE database_id = DB_ID() -- for current db 
+GROUP BY database_id
+                                                                
+                                                                
+
 /* MySql */
 
 -- Returns the database sizes in MB
