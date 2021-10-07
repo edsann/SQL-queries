c0d214a2e63790fab7206ac0eee77014e60230dc
Edoardo Sanna <edoardo.sanna.es@gmail.com>
2021-10-07 11:57:35 +0200

    Create Get-CurrentResources.sql

---
+++ TSQL/Get-CurrentResources.sql
@@ -0,0 +1,20 @@
+-- The following queries return information about currently allocated memory.
+SELECT
+(total_physical_memory_kb/1024) AS Total_OS_Memory_MB,
+(available_physical_memory_kb/1024)  AS Available_OS_Memory_MB
+FROM sys.dm_os_sys_memory;
+
+SELECT  
+(physical_memory_in_use_kb/1024) AS Memory_used_by_Sqlserver_MB,  
+(locked_page_allocations_kb/1024) AS Locked_pages_used_by_Sqlserver_MB,  
+(total_virtual_address_space_kb/1024) AS Total_VAS_in_MB,
+process_physical_memory_low,  
+process_virtual_memory_low  
+FROM sys.dm_os_process_memory; 
+
+-- The following query returns information about current SQL Server memory utilization.
+SELECT
+sqlserver_start_time,
+(committed_kb/1024) AS Total_Server_Memory_MB,
+(committed_target_kb/1024)  AS Target_Server_Memory_MB
+FROM sys.dm_os_sys_info;
